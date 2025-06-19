55233261ca54efdba242133b21715c3e7d698c01
Nacho <revuelta.ig@gmail.com>
2025-06-19 18:07:29 +0200

    fix: setup and build errors for vercel deploys

--- global.d.ts
+++ global.d.ts
@@ -1,3 +1,9 @@
 declare global {
   type ValueOf<T> = T[keyof T]
 }
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
--- src/types/vue.d.ts
+++ src/types/vue.d.ts
@@ -2,6 +2,12 @@ import type { DefineLocaleMessage } from 'vue-i18n'
 
 declare module 'vue' {
   interface ComponentCustomProperties {
-    $t: (key: keyof DefineLocaleMessage) => string
+    $t: (key: string | number) => string
   }
+}
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
 }
